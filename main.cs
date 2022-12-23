e3dfe1097aa03e162a0e95d71232fb569ad74ef9
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-23 03:44:44 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,25 @@
+// Created by: Timothy Manwell
+// Created on: Dec 2022
+//
+// This program Calculates Pi
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var counter = 0.00;
+        int timesThroughLoop;
+        var piAnswer = 0.00;
+
+        Console.Write("Enter a positive or negative integer to calculate Pi. The  the integer, the more acurate the result: ");
+        timesThroughLoop = Convert.ToInt32(Console.ReadLine());
+        while (counter < timesThroughLoop)
+        {
+            piAnswer = piAnswer + (4 / (1 + 2 * counter)) * Math.Pow(-1, counter);
+    counter++;
+        }
+         Console.Write(piAnswer);
+        Console.Write("\nDone");
+    }
+}
